3b381fdb2fa80649994c92f8d44e54e41ad9abfb
Thibaut Vercueil <hello@tibo.space>
2025-04-25 10:20:47 +0100

    fix benchmark code

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,12 +1,12 @@
 use std::path::Path;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use libreeb::RawFileReader;
 
 pub fn evt3_decode_benchmark(c: &mut Criterion) {
     c.bench_function("evt3 decode", |b| {
         b.iter(|| {
-            let path = Path::new("data/openeb/gen4_evt2_hand.raw");
+            let path = Path::new("data/openeb/gen4_evt3_hand.raw");
             let mut reader =
                 RawFileReader::new(Path::new(&path)).expect("Failed to open test file");
             let event_iterator = reader.read_events();
